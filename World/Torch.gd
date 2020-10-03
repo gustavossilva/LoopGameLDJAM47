740b2bb0578a7a5ad668f6296e3e6b541ff9f052
Gustavo Silva <xgustavossilva@gmail.com>
2020-10-03 13:25:30 -0300

    Fix light ilummination

--- World/Torch.gd
+++ World/Torch.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-onready var light = $Light2D
+onready var farLight = $FarLight
+onready var mediumLight = $MediumLight
 onready var timer = $Timer
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if timer.time_left == 0:
-		light.energy = rand_range(0.8,1)
+		farLight.energy = rand_range(0.9,1.1)
+		mediumLight.energy = rand_range(1.0,1.2)
 		timer.start()
