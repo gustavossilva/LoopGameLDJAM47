133af651e6eabcc0c0fb2eb7978593ed55eb624b
Gustavo Silva <xgustavossilva@gmail.com>
2020-10-02 21:58:36 -0300

    Add background, props type and StartMenu

---
+++ Menus/StartMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	VisualServer.set_default_clear_color(Color.black)
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://FirstRoom.tscn")
+
+func _on_Button2_pressed():
+	get_tree().quit()
