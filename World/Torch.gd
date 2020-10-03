0bf8e20dec3d5db730c377be8ca46f711322c468
Gustavo Silva <xgustavossilva@gmail.com>
2020-10-03 12:24:55 -0300

    Add event system

--- World/Torch.gd
+++ World/Torch.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var light = $Light2D
 onready var timer = $Timer
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if timer.time_left == 0:
 		light.energy = rand_range(0.8,1)
 		timer.start()
