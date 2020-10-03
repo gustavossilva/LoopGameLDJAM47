18891f0eb30e0ba78104ed8fb3859bde41dcb4da
Guilherme de Souza Silva <guilherme.souza.silva95@gmail.com>
2020-10-03 13:19:58 -0300

    Improve player animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,15 +8,19 @@ const ACCELERATION = 200
 const FRICTION = 100
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	if(input_vector != Vector2.ZERO):
-		animationPlayer.play("RunRight")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = input_vector.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
