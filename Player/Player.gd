b19bae031b9a26ac6080cff0afda56116b3785bb
Guilherme de Souza Silva <guilherme.souza.silva95@gmail.com>
2020-10-03 13:58:34 -0300

    Fix player moves

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
-const MAX_SPEED = 100
-const ACCELERATION = 200
-const FRICTION = 100
+export(int) var MAX_SPEED = 75
+const ACCELERATION = 500
+const FRICTION = 1000
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -15,11 +15,12 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	if(input_vector != Vector2.ZERO):
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = input_vector.move_toward(Vector2.ZERO, FRICTION * delta)
