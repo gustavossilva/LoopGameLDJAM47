5d4f0654f6f1a82d0dbbe87911744925d0df6d92
Guilherme de Souza Silva <guilherme.souza.silva95@gmail.com>
2020-10-03 18:05:21 -0300

    Add player attack and input key map for attack

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+enum {
+	MOVE,
+	ATTACK
+}
 
 var velocity = Vector2.ZERO
 
@@ -11,7 +15,19 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+var state = MOVE
+
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move(delta)
+		ATTACK:
+			attack(delta)
+
+func move(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,9 +35,20 @@ func _physics_process(delta):
 	if(input_vector != Vector2.ZERO):
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = input_vector.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+		
+func attack(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func on_finished_attack():
+	state = MOVE
