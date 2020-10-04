9d48aad3ff6bc07c24520207d7c7ec4bb99d8a6a
Gustavo Silva <xgustavossilva@gmail.com>
2020-10-04 13:44:57 -0300

    Add dialogs to firstRoom

--- GameManager.gd
+++ GameManager.gd
@@ -1,8 +1,33 @@
 extends Node
 
+enum deathTypes {
+	NONE, FIRE, SWORD, EARTH,  EYE
+}
+
 var isInteracting = false setget set_interacting
 var isDead = false setget set_isDead
 var loopTimes = 0
+var actualDeathType = deathTypes.NONE
+var firedeath = 0
+var sworddeath = 0
+var earthdeath = 0
+var eyedeath = 0
+
+func set_fire_death(value):
+	firedeath += 1
+	actualDeathType = deathTypes.FIRE
+
+func set_sword_death():
+	sworddeath += 1
+	actualDeathType = deathTypes.SWORD
+
+func set_earth_death():
+	earthdeath += 1
+	actualDeathType = deathTypes.EARTH
+
+func set_eye_death():
+	eyedeath += 1
+	actualDeathType = deathTypes.EYE
 
 func set_interacting(value):
 	isInteracting = value
--- UI/UI.gd
+++ UI/UI.gd
@@ -1,6 +1,46 @@
 extends CanvasLayer
 
 onready var bookDialog = $HintOne
+onready var adventurerFirst = $AdventurerFirst
+onready var adventurerDeathFire1 = $AdventurerDeathFire1
+onready var adventurerDeathFire2 = $AdventurerDeathFire2
+onready var adventurerSword1 = $AdventurerSword1
+onready var adventurerSword2 = $AdventurerSword2
+onready var adventurerEye1 = $AdventurerEye1
+onready var adventurerEye2 = $AdventurerEye2
+onready var adventurerEarthshake1 = $AdventurerEarthshake1
+onready var adventurerEarthshake2 = $AdventurerEarthshake2
+onready var timer = $Timer
+
+var showDialog = true
+
+func _process(delta):
+	if GameManager.loopTimes == 0 and showDialog and timer.time_left == 0:
+		adventurerFirst.show_dialog()
+		showDialog = false
+	if GameManager.loopTimes != 0 and showDialog and timer.time_left == 0:
+		match(GameManager.actualDeathType):
+			GameManager.deathTypes.FIRE:
+				if (GameManager.firedeath <= 1):
+					adventurerDeathFire1.show_dialog()
+				else:
+					adventurerDeathFire2.show_dialog()
+			GameManager.deathTypes.EARTH:
+				if (GameManager.firedeath <= 1):
+					adventurerEarthshake1.show_dialog()
+				else:
+					adventurerEarthshake2.show_dialog()
+			GameManager.deathTypes.SWORD:
+				if (GameManager.firedeath <= 1):
+					adventurerSword1.show_dialog()
+				else:
+					adventurerSword2.show_dialog()
+			GameManager.deathTypes.EYE:
+				if (GameManager.firedeath <= 1):
+					adventurerEye1.show_dialog()
+				else:
+					adventurerEarthshake2.show_dialog()
+		showDialog = false
 
 func _on_Book_open_book():
 	bookDialog.play_dialog()
