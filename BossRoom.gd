9bc7b5153118bf73555ada3022602f393bf2b6e4
Gustavo Silva <xgustavossilva@gmail.com>
2020-10-04 16:55:05 -0300

    Add destructable area

--- BossRoom.gd
+++ BossRoom.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 onready var initGameTimer = $InitGameTimer
 onready var fireAttackAnimation = $YSort/FireAttack/FireAttackAnimation
+onready var hiddenRoom1 = $Background/HiddenRoom1
+onready var hiddenRoom2 = $Background/HiddenRoom2
+onready var brokenWall = $Background/BrokenWall
+onready var rightDestructableArea = $RightDestructableArea
 
 func _ready():
 	VisualServer.set_default_clear_color(Color.black)
@@ -17,3 +21,11 @@ func _on_Boss1_hide():
 
 func _on_InitGameTimer_timeout():
 	fireAttackAnimation.play("FireWave")
+
+
+func _on_RightDestructableArea_body_entered(body):
+	hiddenRoom1.visible = true
+	hiddenRoom2.visible = true
+	brokenWall.visible = false
+	rightDestructableArea.queue_free()
+	pass
