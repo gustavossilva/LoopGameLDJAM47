15bcf776a84558567b7e76ba5cef93dc4e86f18a
Guilherme de Souza Silva <guilherme.souza.silva95@gmail.com>
2020-10-04 14:42:31 -0300

    Add hitbox to the player

--- Player/Player.gd
+++ Player/Player.gd
@@ -16,9 +16,11 @@ onready var footParticle = $FootParticle2D
 onready var lightSprite = $Light
 onready var dieSound = $SfxDie
 onready var chargedSound = $SfxCharged
+onready var spinSound = $sfxSpin
 
 var dieSoundPlaying = false
 var chargedIsPlaying = false
+var spinSoundIsPlaying = false
 var state = MOVE
 var velocity = Vector2.ZERO
 var timePressingAttack = 0
@@ -96,10 +98,14 @@ func attack(_delta):
 	
 func charge_attack(delta):
 	chargedIsPlaying = false
+	if !spinSoundIsPlaying:
+		spinSoundIsPlaying = true
+		spinSound.play()
 	animationState.travel("ChargeAttack")
 	move(delta)
 	
 func on_finished_attack():
+	spinSoundIsPlaying = false
 	state = MOVE
 
 func reset_game():
