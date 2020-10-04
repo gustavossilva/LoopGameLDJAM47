70bb2eb3c078930426d530737f3a6af8f4cd8050
Guilherme de Souza Silva <guilherme.souza.silva95@gmail.com>
2020-10-04 17:42:07 -0300

    Add menu image and new buttons

--- Menus/StartMenu.gd
+++ Menus/StartMenu.gd
@@ -8,3 +8,11 @@ func _on_Button_pressed():
 
 func _on_Button2_pressed():
 	get_tree().quit()
+
+
+func _on_ToolButton_pressed():
+	var _changescene = get_tree().change_scene("res://FirstRoom.tscn")
+
+
+func _on_ToolButton2_pressed():
+	get_tree().quit()
